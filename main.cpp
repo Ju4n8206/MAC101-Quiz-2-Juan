efa6cef709691bbef803f7234adf27401a69439f
Ju4n8206 <Juandiegobg8206@gmail.com>
2025-10-23 19:15:06 -0400

    Add FizzBuzz game functionality
    Implemented FizzBuzz logic based on user input.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int main() {
-    //TODO: Create int var and cout and cin commands
+    int range;
+    cout << "Welcome to the FizzBuzz game! Please choose a number: ";
+    cin >> range;
 
+    for (int i = 1; i <= range; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
+            cout << "FizzBuzz" << endl;
+        else if (i % 3 == 0)
+            cout << "Fizz" << endl;
+        else if (i % 5 == 0)
+            cout << "Buzz" << endl;
+        else
+            cout << i << endl;
+    }
 
-    // TODO: Write a for loop that runs from 1 to limit
-    
-
-        // TODO: Use if/else statements to apply FizzBuzz logic
-
-    
-
+    return 0;
+}
     return 0;
 }
